334d8b12a4d5a584fe4c2bcc67306f61b4215549
falken01 <jab.pio@wp.pl>
2021-06-20 13:59:22 +0200

    Created a header

---
+++ src/components/CarsHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <v-app-bar color="dark accent-4" dense dark justify-sm-space-between>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-toolbar-title>Wypożyczalnia supersamochodów</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn depressed color="primary" class="mr-4"> Zaloguj </v-btn>
+      <v-btn depressed color="primary" class="mr-4"> Zarejestruj </v-btn>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CarsHeader",
+  data: () => ({
+    drawer: false,
+    group: null,
+  }),
+};
+</script>
+
+<style scoped></style>
